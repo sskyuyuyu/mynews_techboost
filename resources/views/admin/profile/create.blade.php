315aba7c409f6d5f01b1a9bd460a9e0312201aa0
”sasaki” <ssk925yuiti@gmail.com>
2020-08-15 07:12:37 +0000

    php11_課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,6 +10,23 @@ initial-scale=1">
         <title>Myprofile</title>
     </head>
     <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
+{{-- layouts/profile.blade.phpファイルを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に('プロフィールの新規作成'を埋め込む　--}}
+@section('title','プロフィールの新規作成')
+
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む　--}}
+@section('content')
+      <div class="container">
+          <div class="row">
+              <div class="col-md-8 mx-auto">
+                   <h1>プロフィール作成画面</h1>
+              </div>
+          </div>
+     </div>
+@endsection
+     </body>
 </html>
